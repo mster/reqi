d3f4532c9339d4429ed86e99ba5eb45efdbd9d29
Tyler Laskey <tlaskey3@gmail.com>
2020-03-10 14:17:22 -0700

    add tests

---
+++ tests/integration/client-integration-tests.js
@@ -0,0 +1,45 @@
+'use strict'
+
+const tape = require('tape')
+const ReqiClient = require('../../lib/client')
+const http = require('http')
+let server
+
+tape('setup', function(t) {
+    server = http.createServer((req, res) => {
+        res.writeHead(200)
+        req.pipe(res)
+    })
+    server.listen(3000, () => {
+        server.url = 'http://localhost:' + server.address().port
+        t.end()
+    })
+})
+
+tape('A request should return success status code', function (t) {
+  const client = new ReqiClient()
+  const requestOptions = { url: server.url, method: 'GET' }
+  client.request(requestOptions).then((response) => {
+    t.equal(200, response.statusCode)
+    t.end()
+  }).catch((error) => {
+    t.fail(error)
+    t.end()
+  })
+})
+
+tape('An invalid request should return ReqiError', function (t) {
+  const client = new ReqiClient()
+  const requestOptions = { url: 'http://localhost:9999', method: 'GET' }
+  client.request(requestOptions).then((response) => {
+    t.fail(response)
+    t.end()
+  }).catch((error) => {
+    t.equal('ReqiError', error.name)
+    t.end()
+  })
+})
+
+tape('cleanup', function (t) {
+  server.close(t.end)
+})
