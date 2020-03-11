84c94592178f96a48b3693631e2b89f484cacb96
Tyler Laskey <tlaskey3@gmail.com>
2020-03-10 17:19:22 -0700

    add npm scripts for unit/integration tests, add placeholder for client integration tests

---
+++ tests/integration/client-integration-tests.js
@@ -0,0 +1,10 @@
+'use strict'
+
+var test = require('tape')
+const ReqiError = require('../../lib/error')
+
+//placeholder to allow npm script to run.
+test('1 should equal 1', function(t) {
+    t.equal(1, 1)
+    t.end()
+})
