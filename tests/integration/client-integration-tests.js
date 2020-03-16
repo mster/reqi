1141da30f09f8008e2b3033defd84dbd67effcf9
tlaskey <tlaskey3@gmail.com>
2020-03-15 21:38:02 -0700

    initial commit of mock endpoint for integration testing

--- tests/integration/client-integration-tests.js
+++ tests/integration/client-integration-tests.js
@@ -1,10 +1,30 @@
 'use strict'
 
-var test = require('tape')
+const test = require('tape')
 const ReqiError = require('../../lib/error')
+const MockEndpoint = require('../../lib/mock-endpoint')
+const ReqiClient = require('../../lib/client')
 
-//placeholder to allow npm script to run.
-test('1 should equal 1', function(t) {
-    t.equal(1, 1)
+
+test('sending a GET request should return proper response', function(t) {
+    const initOptions = {
+        redirect: false,
+        redirectCount: 0,
+        retryCodes: [408, 429, 503],
+        retryCount: 3,
+        maxWait: 15, // seconds
+        parseJSON: false
+    }
+
+    const mock = new MockEndpoint()
+    const client = new ReqiClient(initOptions)
+
+    const requestOptions = {
+        url: 'http://localhost/3000/get'
+    }
+
+    const response = client.get(requestOptions)
+    console.log('response ' + response)
+    t.deepEqual(response, requestOptions)
     t.end()
 })
