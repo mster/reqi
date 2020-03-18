ff22d3e32fd02c72465a262ab5b596d2d948a066
tlaskey <tlaskey3@gmail.com>
2020-03-17 18:11:47 -0700

    client-integration tests WIP

--- tests/integration/client-integration-tests.js
+++ tests/integration/client-integration-tests.js
@@ -2,29 +2,43 @@
 
 const test = require('tape')
 const ReqiError = require('../../lib/error')
-const MockEndpoint = require('./mock-endpoint')
 const ReqiClient = require('../../lib/client')
+const http = require('http')
+let server
 
 
-test('sending a GET request should return proper response', function(t) {
-    const initOptions = {
-        redirect: false,
-        redirectCount: 0,
-        retryCodes: [408, 429, 503],
-        retryCount: 3,
-        maxWait: 15, // seconds
-        parseJSON: false
-    }
-
-    const mock = new MockEndpoint()
-    const client = new ReqiClient(initOptions)
-
-    const requestOptions = {
-        url: 'http://localhost/3000/get'
-    }
-
-    const response = client.get(requestOptions)
-    console.log('response ' + response)
-    t.deepEqual(response, requestOptions)
+test('setup', function(t) {
+    server = http.createServer()
+    server.on('request', (req, res) => {
+        res.writeHead(200)
+        req.pipe(res)
+    })
+    server.listen(3000, 'localhost')
+    console.log('Listening on port 3000...')
     t.end()
+})
+
+test('A request should return success status code', function(t) {
+    const client = new ReqiClient()
+    const request = client.request('http://localhost:3000').then((response) => {
+        t.equal(200, response.statusCode)
+    }).catch((error) => {
+        t.fail(error)
+    })
+    t.end()
+})
+
+test('An invalid request should return ReqiError', function(t) {
+    const client = new ReqiClient()
+    const request = client.request('http://localhost:9999').then((response) => {
+        t.fail(response)
+    }).catch((error) => {
+        t.equal('ReqiError', error.name)
+    })
+    t.end()
+})
+
+
+test('cleanup', function (t) {
+    server.close(t.end)
 })
