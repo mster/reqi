814e5c56c024ab06b7167e283c590801eeebf510
tlaskey <tlaskey3@gmail.com>
2020-03-18 17:40:08 -0700

    standard linting fix

--- tests/unit/client-request-tests.js
+++ tests/unit/client-request-tests.js
@@ -1,39 +1,37 @@
 'use strict'
 
 const test = require('tape')
-const ReqiError = require('../../lib/error')
 const ReqiRequest = require('../../lib/client')
 
-test('creating ReqiRequest should create a reqiOptions object containing defaults', function(t) {
-    let client = new ReqiRequest()
-    const actual = client.clientOptions
-    const expected = {
-      redirect: false,
-      redirectCount: 0,
-      retryCodes: [408, 429, 503],
-      retryCount: 3,
-      maxWait: 15,
-      parseJSON: false
-    }
-    t.deepEqual(actual, expected)
-    t.end()
+test('creating ReqiRequest should create a reqiOptions object containing defaults', function (t) {
+  const client = new ReqiRequest()
+  const actual = client.clientOptions
+  const expected = {
+    redirect: false,
+    redirectCount: 0,
+    retryCodes: [408, 429, 503],
+    retryCount: 3,
+    maxWait: 15,
+    parseJSON: false
+  }
+  t.deepEqual(actual, expected)
+  t.end()
 })
 
-test('creating ReqiRequest with initOptions should create a reqiOptions object containing modified defaults', function(t) {
-    const initOptions = {
-        redirect: true
-    }
-    let client = new ReqiRequest(initOptions)
-    const actual = client.clientOptions
-    const expected = {
-      redirect: true,
-      redirectCount: 0,
-      retryCodes: [408, 429, 503],
-      retryCount: 3,
-      maxWait: 15,
-      parseJSON: false
-    }
-    t.deepEqual(actual, expected)
-    t.end()
+test('creating ReqiRequest with initOptions should create a reqiOptions object containing modified defaults', function (t) {
+  const initOptions = {
+    redirect: true
+  }
+  const client = new ReqiRequest(initOptions)
+  const actual = client.clientOptions
+  const expected = {
+    redirect: true,
+    redirectCount: 0,
+    retryCodes: [408, 429, 503],
+    retryCount: 3,
+    maxWait: 15,
+    parseJSON: false
+  }
+  t.deepEqual(actual, expected)
+  t.end()
 })
-
