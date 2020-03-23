be1b0d3c04a2e74bc2489565d07661fcd20b8fb6
mster <michaelsterpka@gmail.com>
2020-03-22 18:05:29 -0700

    tests,opts: - generateOptions checks - clientOptsCheck verifies client state - tests updated to reflect changes

--- tests/unit/test-genOptions.js
+++ tests/unit/test-genOptions.js
@@ -8,7 +8,7 @@ const { generateOptions } = require('../../lib/util')
 tape('generateOptions should return a ReqiError if requestOptions is not present', function (t) {
   const requestOptions = {}
   const actual = generateOptions(requestOptions)
-  const expected = { error: new ReqiError('Error: url unspecified.') }
+  const expected = new ReqiError('Error: url unspecified.')
   t.deepEqual(actual, expected)
   t.end()
 })
@@ -18,7 +18,7 @@ tape('generateOptions should return a ReqiError is url is not present', function
     contentType: 'application/json'
   }
   const actual = generateOptions(requestOptions)
-  const expected = { ...requestOptions, error: new ReqiError('Error: url unspecified.') }
+  const expected = new ReqiError('Error: Invalid or undefined url.', requestOptions)
   t.deepEqual(actual, expected)
   t.end()
 })
@@ -28,16 +28,21 @@ tape('generateOptions should return a ReqiError if url is invalid', function (t)
     url: 'googlecom'
   }
   const actual = generateOptions(requestOptions)
-  const expected = { ...requestOptions, error: new ReqiError('TypeError [ERR_INVALID_URL]: Invalid URL: googlecom') }
+  const expected = new ReqiError('TypeError [ERR_INVALID_URL]: Invalid URL: googlecom')
   t.deepEqual(actual, expected)
   t.end()
 })
 
-tape('generateOptions should return options if given valid requestOptions and addons', function (t) {
+test('generateOptions should return options if given valid requestOptions and addons', function (t) {
+  const transport = require('https')
+  const httpsAgent = new transport.Agent({ id: '666' })
+
   const requestOptions = {
     url: 'https://google.com/test',
-    id: '77b406'
+    id: '000000',
+    agent: httpsAgent
   }
+
   const options = {
     protocol: 'https:',
     method: 'GET',
@@ -46,9 +51,9 @@ tape('generateOptions should return options if given valid requestOptions and ad
     port: 443,
     path: '/test',
     headers: {},
-    agent: undefined,
+    agent: httpsAgent,
     reqCount: { retry: 0, redirect: 0 },
-    id: '77b406'
+    id: '000000'
   }
 
   const actual = generateOptions(requestOptions)
