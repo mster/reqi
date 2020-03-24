21fda7a362d84aaefe9596736de2b14858ff2327
mster <michaelsterpka@gmail.com>
2020-03-23 18:23:55 -0700

    integration: test mutable clientOpts

--- tests/integration/test-client.js
+++ tests/integration/test-client.js
@@ -41,6 +41,37 @@ tape('An invalid request should return ReqiError', function (t) {
   })
 })
 
+tape('changing client options should change request behavior.', async function (t) {
+  const client = new ReqiClient()
+  const requestOptions = { url: server.url }
+
+  let firstRes
+  try {
+    firstRes = await client.request(requestOptions)
+  } catch (error) {
+    firstRes = error
+  }
+
+  client.clientOptions.retryCodes = 200
+  client.clientOptions.retry = 3
+
+  let secondRes
+  try {
+    secondRes = await client.request(requestOptions)
+  } catch (error) {
+    secondRes = error
+  }
+
+  const firstRetry = (firstRes.requestOptions && firstRes.requestOptions.reqCount) ? firstRes.requestOptions.reqCount.retry : null
+  const secondRetry = (secondRes.requestOptions && secondRes.requestOptions.reqCount) ? secondRes.requestOptions.reqCount.retry : null
+
+  const actual = [firstRetry, secondRetry]
+  const expected = [0, 3]
+
+  t.deepEquals(actual, expected)
+  t.end()
+})
+
 tape('cleanup', function (t) {
   server.close(t.end)
 })
